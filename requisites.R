68960ff9d98358125e88ced0b60726463cfaf667
Tom <106226072+cardiotommy@users.noreply.github.com>
2023-10-09 14:16:22 +1100

    Create requisites.R

---
+++ requisites.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(readxl)
+library(vroom)
+library(janitor)
+library(REDCapR)
+uri <- "https://redcap.unsw.edu.au/api/"
+token <- "9CE8EE19432714288DB4F92F2E1C5D60"
+id_token <- "16C7BC58EE069F6BFAFCF5FC7A559E24"
